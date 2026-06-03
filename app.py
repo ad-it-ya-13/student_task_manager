4cdf125626de7120b27b338ba4614eb30a9bce2c
ad-it-ya-13 <aditya8877111@gmail.com>
2026-06-03 12:31:42 +0530

    Update app.py and layout design

--- app.py
+++ app.py
@@ -76,6 +76,9 @@ def home():
     cursor.execute("SELECT COUNT(*) FROM tasks")
     total_tasks = cursor.fetchone()[0]
 
+    cursor.execute("SELECT COUNT(*) FROM student_tasks")
+    total_tasks = cursor.fetchone()[0]
+
     cursor.close()
     connection.close()
 
